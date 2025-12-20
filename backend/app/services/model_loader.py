78a9469b8dd62f4386cd4b09c67d89146449e955
keya-sonaiya <keyasonaiya.266@gmail.com>
2025-12-20 23:00:24 +0530

    update

--- backend/app/services/model_loader.py
+++ backend/app/services/model_loader.py
@@ -1,18 +1,17 @@
-# backend/app/services/model_loader.py
 import os
 from tensorflow import keras
+from dotenv import load_dotenv
 
-# Build absolute path to model in backend/model/
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))      # backend/app/services
-BACKEND_DIR = os.path.dirname(os.path.dirname(THIS_DIR))   # backend
+load_dotenv()
 
-MODEL_PATH = os.path.join(BACKEND_DIR, "model", "best_model.h5")
+MODEL_PATH = os.getenv("MODEL_PATH")
 CLASS_NAMES = ["NORMAL", "PNEUMONIA"]
 
 def load_model_and_classes():
-    print("📥 Loading trained pneumonia model from:", MODEL_PATH)
-    if not os.path.exists(MODEL_PATH):
+    if not MODEL_PATH or not os.path.exists(MODEL_PATH):
         raise FileNotFoundError(f"Model not found at {MODEL_PATH}")
+
+    print(f"📥 Loading trained pneumonia model from: {MODEL_PATH}")
     model = keras.models.load_model(MODEL_PATH)
     print("✅ Model loaded!")
     return model, CLASS_NAMES
